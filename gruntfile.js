6087b49d655f74fdd7f91315be61435bf2c3e6a7
Alejandro <alejcerro@gmail.com>
2015-05-22 19:08:11 -0500

    branching out

---
+++ gruntfile.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Alejandro on 5/22/2015.
+ */
+module.exports = function(grunt) {
+
+  //Project Configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  //Load the plugin that provides the "uglify" task
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //default tasks
+  grunt.registerTask('default', ['uglify']);
+};
