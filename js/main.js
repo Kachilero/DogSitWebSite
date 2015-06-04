c244591456a9343e38a87870876aa55430c1196e
Kachilero <alejcerro@gmail.com>
2015-06-03 22:23:39 -0500

    Maps Work!

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@
 //$(function () {
 //  alert('Welcome to Doggy Day Care! We love dogs');
 //});
+
+var map;
+function loadMap() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(40.748817, -73.985428)
+  };
+  var mapid = document.getElementById('mapped');
+  map = new google.maps.Map(mapid, mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', loadMap());
